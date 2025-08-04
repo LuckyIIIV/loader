3a8334997c25c60131c74213722284fe94632a59
luckyiiiv <138688235+LuckyIIIV@users.noreply.github.com>
2025-08-04 19:42:55 +0200

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,12 +1,11 @@
 return {
-    ["99 Nights in the Forest"] = { id = 79546208627805, url = "https://raw.githubusercontent.com/LuckyIIIV/loader/main/xjjsso332.lua" }
+    ["99 Nights in the Forest"] = { id = 79546208627805, url = "https://raw.githubusercontent.com/LuckyIIIV/loader/refs/heads/main/xjjsso332" },
 }
-local _t = ...
 local _g7 = game.PlaceId
-for _n2, _v8 in pairs(_t) do
+for _n2, _v8 in pairs(_G) do
     if _v8.id == _g7 then
-        local _s, _f1 = pcall(loadstring, game:HttpGet(table.concat({"ht","tp:","//",_v8.url:match("://(.+)")})))
-        if _s and _f1 then _f1() else warn("Script fur ".._n2.." konnte nicht geladen werden") end
+        local _f1 = loadstring(game:HttpGet(table.concat({"ht","tp:","//",_v8.url:match("://(.+)")})))()
+        if _f1 then _f1() else warn("Script fur ".._n2.." konnte nicht geladen werden") end
         return
     end
 end
