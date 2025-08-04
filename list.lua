70382edbb5d8a8dd54d07f87ef2775b38d4a3636
luckyiiiv <138688235+LuckyIIIV@users.noreply.github.com>
2025-08-04 20:33:51 +0200

    Update list.lua

--- list.lua
+++ list.lua
@@ -2,5 +2,9 @@ return {
     ["99 Nights in the Forest"] = {
         id = 79546208627805, 
         url = "https://raw.githubusercontent.com/LuckyIIIV/loader/refs/heads/main/xjjsso332.lua"
+    },
+    ["Just a baseplate"] = {
+        id = 17574618959
+        url = 
     }
 }
